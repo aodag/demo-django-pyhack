302dae0e308dc97aebf9e26d649f9c0f3bfe974f
Atsushi Odagiri <aodagx@gmail.com>
2025-01-17 23:39:06 +0900

    model

--- pyhackregister/models.py
+++ pyhackregister/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class PyhackEvent(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    hold_dateime = models.DateTimeField()
+
+
+class PyhackParticipant(models.Model):
+    event = models.ForeignKey(PyhackEvent, on_delete=models.CASCADE)
+    username = models.CharField(max_length=255)
