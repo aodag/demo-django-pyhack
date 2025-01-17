e36c5b5ebaa6f631ed08a127e7a9bbff280da95a
Atsushi Odagiri <aodagx@gmail.com>
2025-01-17 23:32:28 +0900

    tests

--- pyhackregister/tests.py
+++ pyhackregister/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+class HelloTest(TestCase):
+
+    def test_hello(self):
+        result = self.client.get("/hello/")
+        self.assertEqual(result.context["message"], "Hello, world!")
--- pyhackregister/views.py
+++ pyhackregister/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def hello(request):
     context = {
         "message": "Hello, world!",
